904519aec68b20612bbf9f7c44fe036e53fd2c41
hansithareddy45 <hansithareddy45@gmail.com>
2026-03-03 18:15:10 +0530

    Initial commit - OOPSBannerApp

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
-public class OOPSBannerApp {
+ public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+ 
+        System.out.println(" ******** " + " ******** " + " ******** " + " ******** ");
+ 
+        System.out.println("*        *" + "*        *" + "*        *" + "*        ");
+
+        System.out.println("*        *" + "*        *" + "*        *" + "*        ");
+
+        System.out.println("*        *" + "*        *" + " ******** " + " ******** ");
+
+        System.out.println("*        *" + "*        *" + "*         " + "        *");
+
+        System.out.println("*        *" + "*        *" + "*         " + "        *");
+
+        System.out.println(" ******** " + " ******** " + "*         " + " ******** ");
     }
 }
