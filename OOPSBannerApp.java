a3ae5197f1cb9ef0f5f73ee8a2ea0250f70359db
hansithareddy45 <hansithareddy45@gmail.com>
2026-03-10 13:37:03 +0530

    Implemented UC7: Store Character Pattern in a Class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,50 +1,79 @@
-public class OOPSBannerApp {
+  public class OOPSBannerApp{
+    static class CharacterPatternMap {
 
-    public static void main(String[] args) {
+        private final char character;
+        private final String[] pattern;
 
-        String[] o = getO();
-        String[] p = getP();
-        String[] s = getS();
-
-        // Print 7 lines
-        for (int i = 0; i < 7; i++) {
-            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+        public char getCharacter() {
+            return character;
+        }
+        public String[] getPattern() {
+            return pattern;
         }
     }
- 
-    public static String[] getO() {
-        return new String[] {
-                " ******* ",
-                "*       *",
-                "*       *",
-                "*       *",
-                "*       *",
-                "*       *",
-                " ******* "
-        };
+    public static String[] getCharacterPattern(char ch, CharacterPatternMap[] patterns) {
+        for (CharacterPatternMap cp : patterns) {
+            if (cp.getCharacter() == ch) {
+                return cp.getPattern();
+            }
+        }
+        return null;
     }
+    public static void main(String[] args) {
+        String[] oPattern = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+ 
+        String[] pPattern = {
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        String[] sPattern = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+        CharacterPatternMap[] patterns = {
+                new CharacterPatternMap('O', oPattern),
+                new CharacterPatternMap('P', pPattern),
+                new CharacterPatternMap('S', sPattern)
+        };
 
-    public static String[] getP() {
-        return new String[] {
-                " ********",
-                "*       *",
-                "*       *",
-                " ********",
-                "*        ",
-                "*        ",
-                "*        "
-        };
-    }
- 
-    public static String[] getS() {
-        return new String[] {
-                " ******* ",
-                "*        ",
-                "*        ",
-                " ******* ",
-                "        *",
-                "        *",
-                " ******* "
-        };
+        String word = "OOPS";
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = getCharacterPattern(ch, patterns);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
     }
 }
+ 
